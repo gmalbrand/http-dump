7c3e5e0a617777ad1c68b9799ac876d28a0084ae
Guillaume Malbrand <gmalbrand@free.fr>
2022-12-01 15:40:23 +0100

    Add access combined log to stdout

--- src/main.go
+++ src/main.go
@@ -7,6 +7,8 @@ import (
 	"net/http/httputil"
 	"os"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/gmalbrand/http-dump/monitoring"
 )
@@ -19,6 +21,29 @@ const (
 var (
 	version = os.Getenv("HTTP_DUMP_VERSION")
 )
+
+type (
+	loggingResponseData struct{
+		size int
+		status int
+	}
+
+	loggingResponseWriter struct{
+		http.ResponseWriter
+		loggingData *loggingResponseData
+	}
+)
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+    size, err := r.ResponseWriter.Write(b) // write response using original http.ResponseWriter
+    r.loggingData.size += size // capture size
+    return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+    r.ResponseWriter.WriteHeader(statusCode) // write status code using original http.ResponseWriter
+    r.loggingData.status = statusCode // capture status code
+}
 
 func dumpRequest(w http.ResponseWriter, req *http.Request) {
 	// Adding comment to generate a push and another one
@@ -36,10 +61,38 @@ func info(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "{\"api\": \"http-dump\", \"version\": \"%s\"}", version)
 }
 
+func AccessCombinedLog(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time := time.Now().Format("02/Jan/2006:15:04:05 -0700")
+		loggingData := &loggingResponseData{size : 0, status: 0}
+		lw := loggingResponseWriter{ResponseWriter: w, loggingData: loggingData}
+		handler.ServeHTTP(&lw, r)
+
+		referer := r.Referer()
+		if referer == ""{
+			referer = "-"
+		}
+		user,_,_ := r.BasicAuth()
+		if user == "" {
+			user = "-"
+		}
+		hostAddress := "-"
+		addressSlice := strings.Split(r.RemoteAddr, ":")
+		if len(addressSlice) > 2 {
+			hostAddress = strings.Join(addressSlice[:len(addressSlice)-1], ":")
+		}else{
+			hostAddress = addressSlice[0]
+		}
+		log.Printf("%s - %s [%s] \"%s  %s %s\" %d %d \"%s\" \"%s\"", hostAddress, user, time, r.Method, r.URL, r.Proto, loggingData.status, loggingData.size, referer, r.UserAgent())
+    })
+}
+
+
 func main() {
 	// Get parameters
 	port, err := strconv.Atoi(os.Getenv("HTTP_SERVER_PORT"))
-	
+
+	log.SetFlags(0)
 	if err != nil {
 		port = httpDefaultPort
 	}
@@ -53,5 +106,5 @@ func main() {
 	mux.HandleFunc("/dump", dumpRequest)
 	mux.HandleFunc("/info", info)
 	fmt.Printf("Serving requests on port %d\n", port)
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), mux.Server()))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), AccessCombinedLog(mux.Server())))
 }
