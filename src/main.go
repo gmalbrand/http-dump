098582d095ed147294c39c133d53f8a35d41d55b
Guillaume Malbrand <gmalbrand@free.fr>
2022-12-01 15:55:55 +0100

    Refactor main for smaller func

--- src/main.go
+++ src/main.go
@@ -61,6 +61,25 @@ func info(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "{\"api\": \"http-dump\", \"version\": \"%s\"}", version)
 }
 
+func getDefault(value string) string{
+	if value == "" {
+		return "-"
+	}else{
+		return value
+	}
+}
+
+func getRemoteAddress(r *http.Request) string{
+	hostAddress := "-"
+		addressSlice := strings.Split(r.RemoteAddr, ":")
+		if len(addressSlice) > 2 {
+			hostAddress = strings.Join(addressSlice[:len(addressSlice)-1], ":")
+		}else{
+			hostAddress = addressSlice[0]
+		}
+	return hostAddress
+}
+
 func AccessCombinedLog(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		time := time.Now().Format("02/Jan/2006:15:04:05 -0700")
@@ -68,22 +87,12 @@ func AccessCombinedLog(handler http.Handler) http.Handler {
 		lw := loggingResponseWriter{ResponseWriter: w, loggingData: loggingData}
 		handler.ServeHTTP(&lw, r)
 
-		referer := r.Referer()
-		if referer == ""{
-			referer = "-"
-		}
+		referer := getDefault(r.Referer())
+		
 		user,_,_ := r.BasicAuth()
-		if user == "" {
-			user = "-"
-		}
-		hostAddress := "-"
-		addressSlice := strings.Split(r.RemoteAddr, ":")
-		if len(addressSlice) > 2 {
-			hostAddress = strings.Join(addressSlice[:len(addressSlice)-1], ":")
-		}else{
-			hostAddress = addressSlice[0]
-		}
-		log.Printf("%s - %s [%s] \"%s  %s %s\" %d %d \"%s\" \"%s\"", hostAddress, user, time, r.Method, r.URL, r.Proto, loggingData.status, loggingData.size, referer, r.UserAgent())
+		user = getDefault(user)
+
+		log.Printf("%s - %s [%s] \"%s  %s %s\" %d %d \"%s\" \"%s\"", getRemoteAddress(r), user, time, r.Method, r.URL, r.Proto, loggingData.status, loggingData.size, referer, r.UserAgent())
     })
 }
 
@@ -91,8 +100,8 @@ func AccessCombinedLog(handler http.Handler) http.Handler {
 func main() {
 	// Get parameters
 	port, err := strconv.Atoi(os.Getenv("HTTP_SERVER_PORT"))
-
 	log.SetFlags(0)
+	
 	if err != nil {
 		port = httpDefaultPort
 	}
